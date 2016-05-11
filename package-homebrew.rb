c7cfd8fb84620809dbcad9f2b37377da494387a5
Drew Crawford <drew@sealedabstract.com>
2016-05-11 01:59:27 -0500

    Test package-homebrew

--- package-homebrew.rb
+++ package-homebrew.rb
@@ -4,9 +4,9 @@
 class PackageHomebrew < Formula
   desc "No description!  file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
   homepage "No homepage! file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
-  url "https://github.com/AnarchyTools/package-homebrew/releases/download/0.1/package-homebrew-0.1.tar.xz"
+  url "https://github.com/AnarchyTools/package-homebrew/releases/download/0.1/package-homebrew-0.1.atbin.tar.xz"
   version "0.1"
-  sha256 "5f494ef97ea0a0a1dba6682060bf73f5ed8a135e312a56c7e4eb7874c17fafd0  bin/package-homebrew-0.1.atbin.tar.xz"
+  sha256 "13a664babc895ab97a8e9b4af5feb940238e8c2721e45ad883cd2f6293e10420  bin/package-homebrew-0.1.atbin.tar.xz"
 
   def install
     bin.install "package-homebrew.atbin/package-homebrew"
