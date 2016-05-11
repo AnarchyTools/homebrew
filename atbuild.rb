88667d3dfe3d1e85366715b8367bad00e4f78018
Drew Crawford <drew@sealedabstract.com>
2016-05-11 04:05:07 -0500

    Release atbin 1.2

--- atbuild.rb
+++ atbuild.rb
@@ -1,13 +1,14 @@
+
 # Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 class Atbuild < Formula
-  desc "The Anarchy Tools build tool"
-  homepage "http://anarchytools.org"
-  url "https://github.com/AnarchyTools/atbuild/releases/download/1.1/atbuild-1.1-osx.tar.xz"
-  version "1.1"
-  sha256 "4faf6dba6117939dbb8b924952f260e66ba7db1e23e0dfb5f4423be87a0654ed"
+  desc "No description!  file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
+  homepage "No homepage! file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
+  url "https://github.com/AnarchyTools/atbuild/releases/download/1.2/atbuild-1.2-osx.atbin.tar.xz"
+  version "1.2"
+  sha256 "6ab007d01f36919c3dde72f9bba8973e8fa340f1ac911b6be913f0b17f7edd10"
 
   def install
-    bin.install Dir["*"]
+    bin.install "atbuild.atbin/"
   end
 end
