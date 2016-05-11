292baac82975fb5909586f6038f2e33d8e371785
Drew Crawford <drew@sealedabstract.com>
2016-05-11 04:20:51 -0500

    Bump package-homebrew

--- package-homebrew.rb
+++ package-homebrew.rb
@@ -6,9 +6,10 @@ class PackageHomebrew < Formula
   homepage "No homepage! file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
   url "https://github.com/AnarchyTools/package-homebrew/releases/download/0.1/package-homebrew-0.1-osx.atbin.tar.xz"
   version "0.1"
-  sha256 "96b3b57346bd56a66106eed47f544a7d5b1a9a463705576a1b964a5d0944627d"
+  sha256 "773ca840834495866bcff428952cae5d4cd470e3a4bf006ba5a9a81473da750c"
 
   def install
-    bin.install "package-homebrew.atbin/package-homebrew"
+  # We don't need the folder name here; it seems to be supplied automatically
+    bin.install "package-homebrew"
   end
 end
