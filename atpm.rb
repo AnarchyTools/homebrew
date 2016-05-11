3b851fd0078c030a537a52e1085d18e4aad2b14b
Drew Crawford <drew@sealedabstract.com>
2016-05-11 07:02:12 -0500

    Package atpm 1.1

--- atpm.rb
+++ atpm.rb
@@ -1,11 +1,15 @@
+
+# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 class Atpm < Formula
-  desc "The Anarchy Tools package manager"
-  homepage "http://anarchytools.org"
-  url "https://github.com/AnarchyTools/atpm/releases/download/1.0.1/atpm-1.0.1-osx.tar.xz"
+  desc "No description!  file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
+  homepage "No homepage! file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
+  url "https://github.com/AnarchyTools/atpm/releases/download/1.0.1/atpm-1.0.1-osx.atbin.tar.xz"
   version "1.0.1"
-  sha256 "8c6aff78f02972fc6c9a994d205967b11ff2838eba424322206270b2204cb6f0"
+  sha256 "ed5f80b4650d9d829a105800e54166f78d38ba3bf0855b9494d05d5444f46de7"
 
   def install
-    bin.install Dir["*"]
+  # We don't need the folder name here; it seems to be supplied automatically
+    bin.install "atpm"
   end
 end
