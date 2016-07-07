2d06e0ef7deccd00965a578be279b092dfcd191f
Drew Crawford <drew@sealedabstract.com>
2016-07-07 03:26:34 -0500

    Bump package-framework

--- package-framework.rb
+++ package-framework.rb
@@ -4,9 +4,9 @@
 class PackageFramework < Formula
   desc "No description!  file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
   homepage "No homepage! file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
-  url "https://github.com/AnarchyTools/package-framework/releases/download/0.1.1/package_framework-0.1.1-osx.atbin.tar.xz"
-  version "0.1.1"
-  sha256 "1c73586d12ad1fc44ab2146edee9a638ca90565beab1b52274c6e7da4c8defc4"
+  url "https://github.com/AnarchyTools/package-framework/releases/download/1.0/package_framework-1.0-osx.atbin.tar.xz"
+  version "1.0"
+  sha256 "b79537b5b121bb259ccad9a30c691ae4fe26cce174583ad4de7879bbba30dde2"
 
   def install
   # We don't need the folder name here; it seems to be supplied automatically
