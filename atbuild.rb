579d4e6661615c5156155283b0836e80c9708243
Drew Crawford <drew@sealedabstract.com>
2016-07-15 05:29:05 -0500

    Bump atbuild

--- atbuild.rb
+++ atbuild.rb
@@ -4,9 +4,9 @@
 class Atbuild < Formula
   desc "No description!  file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
   homepage "No homepage! file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
-  url "https://github.com/AnarchyTools/atbuild/releases/download/1.4.0/atbuild-1.4.0-osx.atbin.tar.xz"
-  version "1.4.0"
-  sha256 "4ca052024fdfa0a26e34d658ebc2b7bf32f5c9a77891558deac3826564e8c5c5"
+  url "https://github.com/AnarchyTools/atbuild/releases/download/1.5.0/atbuild-1.5.0-osx.atbin.tar.xz"
+  version "1.5.0"
+  sha256 "a915c589e13eb8dbafab0d9c04bb78901b24be6aef9ad1e764288e09c59d6bd7"
 
   def install
   # We don't need the folder name here; it seems to be supplied automatically
