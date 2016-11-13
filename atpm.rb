59b75bef6bd1e443604d9eccf490d94e7f4563c0
Drew Crawford <drew@sealedabstract.com>
2016-11-13 13:59:52 -0600

    bump atpm 1.2.4

--- atpm.rb
+++ atpm.rb
@@ -4,9 +4,9 @@
 class Atpm < Formula
   desc "No description!  file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
   homepage "No homepage! file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
-  url "https://github.com/AnarchyTools/atpm/releases/download/1.2.3/atpm-1.2.3-osx.atbin.tar.xz"
-  version "1.2.3"
-  sha256 "f9fd86d929995157ea6c0734914bfa96bff042406906af0e65d360abd80177e4"
+  url "https://github.com/AnarchyTools/atpm/releases/download/1.2.4/atpm-1.2.4-osx.atbin.tar.xz"
+  version "1.2.4"
+  sha256 "dd9b5c77bd60054446250af3401e6f8527ef415631eed7308ef5a4a56b7fabe1"
 
   def install
   # We don't need the folder name here; it seems to be supplied automatically
