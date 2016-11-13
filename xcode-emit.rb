0b2b5a18a4a3b2f2190d45743f595ddc30d802a4
Drew Crawford <drew@sealedabstract.com>
2016-11-13 14:48:25 -0600

    Bump xcode-emit to 1.1

--- xcode-emit.rb
+++ xcode-emit.rb
@@ -4,9 +4,9 @@
 class XcodeEmit < Formula
   desc "No description!  file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
   homepage "No homepage! file a bug at https://github.com/AnarchyTools/package-homebrew/issues"
-  url "https://github.com/AnarchyTools/xcode-emit/releases/download/1.0/xcode_emit-1.0-osx.atbin.tar.xz"
-  version "1.0"
-  sha256 "bf439b2cf496685f2e970813eedd3d03f2e165078f0cb43a28ae3222d0f77372"
+  url "https://github.com/AnarchyTools/xcode-emit/releases/download/1.1/xcode_emit-1.1-osx.atbin.tar.xz"
+  version "1.1"
+  sha256 "b300cd3d959b88ee5e732c99a779ccab3bdf8dd697bd8deb00c8e9c92d47319e"
 
   def install
   # We don't need the folder name here; it seems to be supplied automatically
